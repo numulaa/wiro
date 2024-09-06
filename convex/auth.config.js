645bfae3232aea54e8f3699ae7d82518b75da2c6
Nurul Mukhlisa <numulaa@gmail.com>
2024-09-07 00:49:50 +0800

    first commit

---
+++ convex/auth.config.js
@@ -0,0 +1,8 @@
+export default {
+  providers: [
+    {
+      domain: "https://deciding-gibbon-37.clerk.accounts.dev",
+      applicationID: "convex", // this should be the same as uid in the clerk
+    },
+  ],
+};
